917810177b45dc45f68913a728f5ff2efdd951d3
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2023-07-05 16:27:28 +0200

    Some style

--- Library.Front/src/app/app.component.ts
+++ Library.Front/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'materialize-css';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Library.Front';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const parallaxElems = document.querySelectorAll('.parallax-container');
+    M.Parallax.init(parallaxElems);
+  }
+  
 }
